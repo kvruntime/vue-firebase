5b6ed8b69242dfffc1e66660e72788a8b3dafda5
kvruntime <kvruntime@gmail.com>
2023-08-30 13:53:11 +0000

    feat(): update packages
    add vuetify
    remove primevue & primeicons

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,9 +10,9 @@ const count = ref(0)
 
 <template>
   <h1>{{ msg }}</h1>
-
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <v-btn variant="tonal">Vuetify Button</v-btn>
+    <button type="button" @click="count++"><v-btn>count is {{ count }}</v-btn></button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
@@ -21,9 +21,8 @@ const count = ref(0)
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
+    starter
   </p>
   <p>
     Install
--- src/main.js
+++ src/main.js
@@ -4,13 +4,17 @@ import App from './App.vue';
 import { VueFire, VueFireAuth } from 'vuefire';
 import { firebaseApp } from './firebase.js';
 import router from './router';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import 'vuetify/dist/vuetify.min.css';
 
 const app = createApp(App);
 app
 	.use(router)
-
+	.use(createVuetify({ components }))
 	.use(VueFire, {
 		firebaseApp,
 		modules: [VueFireAuth()],
 	});
+
 app.mount('#app');
