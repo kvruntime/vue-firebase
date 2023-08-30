2869dd7b4159d0fe16653bb1d62a1e8237da0319
kvruntime <kvruntime@gmail.com>
2023-08-30 17:08:45 +0000

    feat() add registration

--- src/pages/auth/LoginPage.vue
+++ src/pages/auth/LoginPage.vue
@@ -1,28 +1,72 @@
 <script setup>
+import { useFirebaseAuth } from 'vuefire';
+import { signInWithEmailAndPassword  } from 'firebase/auth';
 
+import { ref } from 'vue';
+
+const userInputs = ref({ email: '', password: '' });
+const validations = ref({ error: '', raised: false });
+
+const auth = useFirebaseAuth();
+
+async function login() {
+	try {
+		const credential = await signInWithEmailAndPassword (
+			auth,
+			userInputs.value.email,
+			userInputs.value.password,
+		);
+		console.log(credential);
+	} catch (error) {
+		console.log(error.message);
+		validations.value.error = error.message;
+		validations.value.raised = true;
+	} finally {
+		userInputs.value.email=""
+		userInputs.value.password=""
+	}
+}
 </script>
 <template>
 	<v-sheet class="rounded-0" width="400" height="700">
 		<h1 class="mb-2">Login</h1>
-		<v-form>
-			<v-text-field label="Name" clearable  />
+		<v-form @submit.prevent="login">
+			<v-text-field
+				label="Email"
+				clearable
+				v-model="userInputs.email"
+				type="email"
+				required
+			/>
 			<v-text-field
 				label="Password"
+				type="password"
 				clearable
-			
-				:type="password"
+				v-model="userInputs.password"
 			/>
 
 			<v-btn-group>
-				<v-btn class="bg-red-accent-3" variant="outlined">Reset</v-btn>
-				<v-btn class="bg-green-accent-1" variant="outlined"
-					>Login
-				</v-btn>
+				<v-btn class="bg-red-accent-3" variant="outlined" type="reset"
+					>Reset</v-btn
+				>
+				<v-btn class="bg-green-accent-1" variant="outlined" type="submit"
+					>Login</v-btn
+				>
 			</v-btn-group>
 
-
+			<div>{{ userInputs }}</div>
 		</v-form>
 	</v-sheet>
+
+	<v-snackbar
+  y="top"
+		:timeout="2000"
+		color="blue-grey"
+		rounded="pill"
+		v-model="validations.raised"
+	>
+		Error
+	</v-snackbar>
 </template>
 
 <style></style>
--- src/pages/auth/RegisterPage.vue
+++ src/pages/auth/RegisterPage.vue
@@ -1,24 +1,29 @@
 <script setup>
-import { useFirebaseAuth  } from 'vuefire';
+import { useFirebaseAuth } from 'vuefire';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 import { ref } from 'vue';
 
 const userInputs = ref({ email: '', password: '' });
+const validations = ref({ error: '', raised: false });
 
 const auth = useFirebaseAuth();
 
 async function register() {
-
 	try {
-		const credential = createUserWithEmailAndPassword(
+		const credential = await createUserWithEmailAndPassword(
 			auth,
 			userInputs.value.email,
 			userInputs.value.password,
 		);
 		console.log(credential);
 	} catch (error) {
-		console.error(error);
+		console.log(error.message);
+		validations.value.error = error.message;
+		validations.value.raised = true;
+	} finally {
+		userInputs.value.email=""
+		userInputs.value.password=""
 	}
 }
 </script>
@@ -26,7 +31,13 @@ async function register() {
 	<v-sheet class="rounded-0" width="400" height="700">
 		<h1 class="mb-2">Register</h1>
 		<v-form @submit.prevent="register">
-			<v-text-field label="Name" clearable v-model="userInputs.email" />
+			<v-text-field
+				label="Email"
+				clearable
+				v-model="userInputs.email"
+				type="email"
+				required
+			/>
 			<v-text-field
 				label="Password"
 				type="password"
@@ -35,13 +46,27 @@ async function register() {
 			/>
 
 			<v-btn-group>
-				<v-btn class="bg-red-accent-3" variant="outlined" type="reset">Reset</v-btn>
-				<v-btn class="bg-green-accent-1" variant="outlined" type="submit">Register</v-btn>
+				<v-btn class="bg-red-accent-3" variant="outlined" type="reset"
+					>Reset</v-btn
+				>
+				<v-btn class="bg-green-accent-1" variant="outlined" type="submit"
+					>Register</v-btn
+				>
 			</v-btn-group>
 
-      <div>{{userInputs}}</div>
+			<div>{{ userInputs }}</div>
 		</v-form>
 	</v-sheet>
+
+	<v-snackbar
+  y="top"
+		:timeout="2000"
+		color="blue-grey"
+		rounded="pill"
+		v-model="validations.raised"
+	>
+		Error
+	</v-snackbar>
 </template>
 
 <style></style>
